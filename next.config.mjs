71ae2f624d0b8195591102bc00b30c62d77a5b32
vwcamper77 <gavinferns@hotmail.com>
2025-04-01 00:07:57 +0100

    Fix next.config.mjs to disable JS caching on mobile

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)\\.js",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
-
